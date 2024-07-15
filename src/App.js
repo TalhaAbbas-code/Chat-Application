d08e86a87ba9ea12937bd3407f0e24bfb719ab31
TalhaAbbas-code <talhaabbas058@gmail.com>
2024-07-15 12:03:59 +0500

    pushing file

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '237771df-dd0a-4592-8996-ba622569d315';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
